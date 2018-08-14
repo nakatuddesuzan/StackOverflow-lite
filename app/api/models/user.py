dd535718066452169eabf86f31dbe9489869b208
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-14 19:42:24 +0300

    Set and validate User entries

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -1,9 +1,62 @@
+import re
+import jwt
+from datetime import datetime, timedelta
+from app import app
+
+users_list = []
+
+
 class User(object):
     """Class representing User model"""
 
-    def __init__(self, username, email, password):
+    def __init__(self, user_id, username, email, password):
+        self.user_id = user_id
         self.username = username
         self.email = email
         self.password = password
 
-    
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, pwd):
+        if not pwd:
+            raise Exception("Field can't be empty")
+        if len(pwd) < 8 and len(pwd) > 12:
+            raise Exception(
+                "Weak password \n Password must be 8 characters long ")
+        if not re.search(r'[0-9]', pwd):
+            raise Exception(
+                'Weak password \n Password should have atleast one integer')
+        if pwd.isupper() or pwd.islower() or pwd.isdigit():
+            print(
+                "Weak password \n Either you need to include alphabets or \n try include both letter cases")
+        self._password = pwd
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, value):
+        if not value:
+            raise Exception("Email field can't be empty")
+        if not re.match(r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+$)", value):
+            raise ValueError('Enter Valid Email ID forexample "sue@gmail.com"')
+        self._email = value
+
+    @property
+    def username(self):
+        return self._username
+    @username.setter
+    def username(self, value):
+        if not value:
+            raise Exception("Field can't be empty")
+        if len(value) <= 2:
+            raise Exception("Name too sort \n  Not allowed")
+        if re.compile('[!@#$%^&*:;?><.0-9]').match(value):
+            raise ValueError("Invalid characters not allowed")
+
+        self._username = value
+        
