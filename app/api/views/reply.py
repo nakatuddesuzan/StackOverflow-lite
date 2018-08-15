fcb538a0983888e6b70497db42b0af83ab8e48b5
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-15 23:43:38 +0300

    Transfer answer end-pont to its file [#159808377]

---
+++ app/api/views/reply.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request, jsonify, make_response
+from app.api.models.user import users_list
+from app.api.models.questions import Question, qtns_list
+from app.api.models.user import User
+from app import generate_id
+from app.api.models.reply import Reply
+
+
+answers = Blueprint('answers', __name__)
+
+
+@answers.route('/api/v1/answer/<int:user_id>/<int:qtn_id>', methods=['POST'])
+def post_answer(user_id, qtn_id):
+    if not request.get_json():
+        return make_response(jsonify({"message": "Request should be json"}), 400)
+    reply_desc = request.get_json()['reply_desc']
+    user_id = request.get_json()['user_id']
+    qtn_id = request.get_json()['qtn_id']
+    for user in  users_list:
+        if user_id == user['user_id']:
+            for question in qtns_list:
+                if qtn_id == question['qtn_id']:
+                    answer = Reply(user_id=user_id, qtn_id=qtn_id, reply_desc = reply_desc)
+                    reply_made = User.make_reply(answer)
+                    return jsonify(reply_made), 200   
+        return jsonify({"message": "Question not found"})   
+    return jsonify({"message": "Sign up to be able to ask questions  on this platform"})
