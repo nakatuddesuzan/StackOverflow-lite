59b1026dbddbe69de727fea2874885efb5803148
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-16 01:04:34 +0300

    Create endpoint for deetin a question by id [#159810587]

--- app/api/views/questions.py
+++ app/api/views/questions.py
@@ -7,7 +7,7 @@ from app import generate_id
 
 questions = Blueprint('questions', __name__)
 
-@questions.route('/api/v1/question/<int:user_id>', methods=['POST'])
+@questions.route('/api/v1/questions/<int:user_id>', methods=['POST'])
 def post_question(user_id):
     if not request.get_json():
         return make_response(jsonify({"message": "Request should be json"}), 400)
@@ -26,3 +26,8 @@ def post_question(user_id):
         qtn_made = User.create_qtn(qtn_instance)
         return jsonify(qtn_made), 200
     return jsonify({"message": "Sign up to be able to ask questions  on this platform"})
+
+@questions.route('/api/v1/questions/<int:user_id>/<int:qtn_id>', methods=['DELETE'])
+def del_qtn(qtn_id, user_id):
+    remaining_questions = User.delete_qtn(qtn_id, user_id)
+    return jsonify({'Current_questions': remaining_questions})
