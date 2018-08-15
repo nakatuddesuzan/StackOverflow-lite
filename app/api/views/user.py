580daa03ea43f23d213d5306301b3db6341a99a0
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-15 13:09:37 +0300

    Import function to generate an id[#159787597]

--- app/api/views/user.py
+++ app/api/views/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, make_response
 from app.api.models.user import User, users_list
+from app import generate_id
 
 
 auth = Blueprint('auth', __name__)
@@ -7,15 +8,16 @@ auth = Blueprint('auth', __name__)
 @auth.route('/api/v1/users/signup', methods=['POST'])
 def register():
     if not request.get_json():
-        return make_response(jsonify({"message": "Request dhould be json"}), 400)
+        return make_response(jsonify({"message": "Request should be json"}), 400)
     username = request.get_json()['username']
     email = request.get_json()['email']
     password = request.get_json()['password']
-
+    user_id = generate_id(users_list)
     user = {
             'username' : username,
             'email': email,
-            'password': password
+            'password': password,
+            'user_id': user_id
         } 
     
     User('user_id', username=username, email=email, password=password)
