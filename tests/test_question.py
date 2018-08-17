1a704c4188462c0ccb92f0ce194758ae0db90bd9
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-17 19:07:08 +0300

    Fix end-point status code

--- tests/test_question.py
+++ tests/test_question.py
@@ -16,36 +16,41 @@ class TestQuestion(BaseTestCase):
         """
         with self.client:
             response = self.post_question(1, "flask", "python", "importing files")
+            print(response)
             self.assertTrue(response.content_type == 'application/json')
 
     def test_json_data_error_response(self):
         """
             Test error message if not json data provided
         """
-        response = self.post_question(1, "flask", "python", "importing files")
-        data = json.loads(response.data.decode())
-        self.assertNotEqual(data.get('message'), "Request should be json")
+        with self.client:
+            response = self.post_question(1, "flask", "python", "importing files")
+            data = json.loads(response.data.decode())
+            self.assertNotEqual(data.get('message'), "Request should be json")
 
     def test_json_data_error_response_code(self):
         """
             Test response code if not json data provided
         """
-        response = self.post_question(1, "flask", "python", "importing files")
-        self.assertNotEqual(response.status_code, 400)
+        with self.client:
+            response = self.post_question(1, "flask", "python", "importing files")
+            self.assertNotEqual(response.status_code, 400)
 
     def test_question_added_successfully(self):
         """
             Test for successful posting of a questsion
         """
-        self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-        response = self.post_question(1, "flask", "python", "importing files")
-        self.assertEqual(response.status_code, 200)
+        with self.client:
+            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
+            response = self.post_question(1, "flask", "python", "importing files")
+            self.assertEqual(response.status_code, 201)
 
     def test_auth_to_post_question(self):
         """
             Test for an 
             unathorized user can post a questsion
         """
-        response = self.post_question(1, "flask", "python", "importing files")
-        data = json.loads(response.data.decode())
-        self.assertEqual(data.get('message'), "Sign up to be able to ask questions  on this platform")
+        with self.client:
+            response = self.post_question(1, "flask", "python", "importing files")
+            data = json.loads(response.data.decode())
+            self.assertEqual(data.get('message'), "Sign up to be able to ask questions  on this platform")
