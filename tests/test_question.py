401d6198e37393d6728faf8584db90e7e83289b8
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-18 01:34:29 +0300

    Fix post question method [Finishes #159856545]

--- tests/test_question.py
+++ tests/test_question.py
@@ -54,3 +54,61 @@ class TestQuestion(BaseTestCase):
             response = self.post_question(1, "flask", "python", "importing files")
             data = json.loads(response.data.decode())
             self.assertEqual(data.get('message'), "Sign up to be able to ask questions  on this platform")
+    
+    def test_get_all_questions(self):
+        """test if user can retrieve all questions"""
+
+        with self.client:
+            self.post_question(1, "flask", "python", "importing files")
+            self.post_question(1, "flask", "python", "importing files")
+            response = self.get_all_questions()
+            self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_one_questions(self):
+        """test if user can retrieve one question"""
+
+        with self.client:
+            self.post_question(1, "flask", "python", "importing files")
+            response = self.get_one_question()
+            self.assertEqual(response.status_code, 200)
+
+    def test_delete_question(self):
+        """test if user can delete one question"""
+
+        with self.client:
+            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
+            self.post_question(1, "flask", "python", "importing files")
+            response = self.delete_question(1, 1)
+            self.assertEqual(response.status_code, 200)
+    
+    def test_update_question(self):
+        """test if user can update a question"""
+
+        with self.client:
+            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
+            self.post_question(1, "flask", "python", "importing files")
+            response = self.update_question(1, 1, "not working", "CSS", "chjushxhxbh" )
+            print(response)
+            self.assertEqual(response.status_code, 200)
+
+    def test_json_data_error_response_for_update_question(self):
+        """
+            Test error message if not json data provided
+        """
+        with self.client:
+            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
+            self.post_question(1, "flask", "python", "importing files")
+            response = self.update_question(1, 1, "not working", "CSS", "chjushxhxbh" )
+            data = json.loads(response.data.decode())
+            self.assertNotEqual(data.get('message'), "Request should be json")
+
+    def test_json_data_error_response_code_for_update_question(self):
+        """
+            Test response code if not json data provided
+        """
+        with self.client:
+            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
+            self.post_question(1, "flask", "python", "importing files")
+            response = self.update_question(1, 1, "not working", "CSS", "chjushxhxbh" )
+            self.assertNotEqual(response.status_code, 400)
+
