bf798d35386d28edcef15249acf4a19ef9220699
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-08-21 00:09:17 +0300

    Write tests for authenticated questions routes

--- tests/test_question.py
+++ tests/test_question.py
@@ -15,8 +15,6 @@ class TestQuestion(BaseTestCase):
             Test for json question data
         """
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            response = self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             response = self.post_question(token,  1,"flask", "python", "importing files")
             self.assertTrue(response.content_type == 'application/json')
@@ -26,8 +24,6 @@ class TestQuestion(BaseTestCase):
             Test error message if not json data provided
         """
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            response = self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             response = self.post_question(token,  1,"flask", "python", "importing files")
             data = json.loads(response.data.decode())
@@ -38,8 +34,6 @@ class TestQuestion(BaseTestCase):
             Test response code if not json data provided
         """
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            response = self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             response = self.post_question(token,  1,"flask", "python", "importing files")
             self.assertNotEqual(response.status_code, 400)
@@ -49,8 +43,6 @@ class TestQuestion(BaseTestCase):
             Test for successful posting of a questsion
         """
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            response = self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             response = self.post_question(token,  1,"flask", "python", "importing files")
             self.assertEqual(response.status_code, 201)
@@ -61,10 +53,7 @@ class TestQuestion(BaseTestCase):
             unathorized user can post a questsion
         """
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            response = self.login_user("sue@gmail.com", "Bootcamp11")
-            token = self.get_token()
-            response = self.post_question(token,  1,"flask", "python", "importing files")
+            response = self.post_question("id",  1,"flask", "python", "importing files")
             data = json.loads(response.data.decode())
             self.assertEqual(data.get('message'),"Invalid token. Please log in again.")
 
@@ -72,21 +61,16 @@ class TestQuestion(BaseTestCase):
         """test if user can retrieve all questions"""
 
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             self.post_question(token,  1, "flask", "python", "importing files")
             response = self.get_all_questions(token)
             data = json.loads(response.data.decode())
-            print(data)
-            self.assertEqual(response.status_code, 200)
+            self.assertTrue(data['questions'])
 
     def test_retrieve_one_questions(self):
         """test if user can retrieve one question"""
 
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             self.post_question(token,  1, "flask", "python", "importing files")
             response = self.get_one_question(token)
@@ -96,22 +80,22 @@ class TestQuestion(BaseTestCase):
         """test if user can delete one question"""
 
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             self.post_question(token,  1, "flask", "python", "importing files")
             response = self.delete_question(token, 1, 1)
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['questions'], [])
             self.assertEqual(response.status_code, 200)
     
     def test_update_question(self):
         """test if user can update a question"""
 
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             self.post_question(token,  1, "flask", "python", "importing files")
             response = self.update_question(token, 1, 1, "not working", "CSS", "chjushxhxbh" )
+            data = json.loads(response.data.decode())
+            self.assertTrue(data['Updated'])
             self.assertEqual(response.status_code, 200)
 
     def test_json_data_error_response_for_update_question(self):
@@ -119,8 +103,6 @@ class TestQuestion(BaseTestCase):
             Test error message if not json data provided
         """
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             self.post_question(token,  1, "flask", "python", "importing files")
             response = self.update_question(token, 1, 1, "not working", "CSS", "chjushxhxbh" )
@@ -132,9 +114,46 @@ class TestQuestion(BaseTestCase):
             Test response code if not json data provided
         """
         with self.client:
-            self.register_user("sue", "sue@gmail.com", "Bootcamp11")
-            self.login_user("sue@gmail.com", "Bootcamp11")
             token = self.get_token()
             self.post_question(token,  1, "flask", "python", "importing files")
             response = self.update_question(token, 1, 1, "not working", "CSS", "chjushxhxbh" )
             self.assertNotEqual(response.status_code, 400)
+    
+    def test_delete_all_questions(self):
+        """test if all questions can be deleted"""
+        with self.client:
+            token = self.get_token()
+            self.post_question(token,  1, "flask", "python", "importing files")
+            response = self.delete_all_questions(token)
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['Replies left'], [])
+
+    def test_delete_all_questions_with_empty_qtns_list(self):
+        """
+            Test for delete all questions for abn already empty list
+        """
+        with self.client:
+            token = self.get_token()
+            response = self.delete_all_questions(token)
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['message'], 'List empty')
+    
+    def test_update_missing_questions(self):
+        """
+            Test for trying to delete questions that don't exist
+        """
+        with self.client:
+            token = self.get_token()
+            response = self.update_question(token, 2, 1, "not working", "CSS", "chjushxhxbh" )
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['Updated'], "Question not found")
+    
+    def test_delete_missing_question(self):
+        """
+            Test for trying to delete a question that doesn.t exist
+        """
+        with self.client:
+            token = self.get_token()
+            response = self.delete_question(token, 1, 1)
+            data = json.loads(response.data.decode())
+            self.assertEqual(data['message'], "No questions found")
